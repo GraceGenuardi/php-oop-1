098952adef4fcafd6d27ecc843399c2e37b49a75
GraceGenuardi <graziagenuardi96@gmail.com>
2023-03-30 15:33:43 +0200

    FIX variabili con nome

--- index.php
+++ index.php
@@ -17,19 +17,24 @@
      public $genere;
      public $regista;
 
+
     //3.Definizione del costruttore
-    public function __construct($title, $director, $year) {
+    public function __construct($titolo, $genere, $regista) {
         $this->titolo = $titolo;
         $this->genere = $genere;
         $this->regista = $regista;
       }
   
-    //4.Definizione di un metodo
 
+    //4.Definizione di un metodo
+    public function getInfo() {
+        return "Titolo: " . $this->titolo . ", Genere: " . $this->genere . ", Regista: " . $this->regista;
+      }
   }
  
 
  //5.Istanza di due oggetti Movie  
+
 
 ?>
 
@@ -45,12 +50,12 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>PHP OOOP</title>
 </head>
-<body class="bg-primary">
+<body class="bg-dark">
 
 
    <div class="col-11 d-flex justify-content-center mt-5">
 
-      <h1 class="text-warning pt-5">PHP O O P </h1>
+      <h1 class="text-danger pt-5">PHP OOP </h1>
 
     </div>
 </body>
